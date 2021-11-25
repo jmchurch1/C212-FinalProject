e60b6f7b0f9d0f2b8f52a5a3d3e9fbf86e59ec3c
Josh <josh.church.josh@gmail.com>
2021-11-25 17:54:23 -0500

    StoreAction enum set up, and switch statement for store set up

--- src/edu/iu/c212/places/Store.java
+++ src/edu/iu/c212/places/Store.java
@@ -20,40 +20,42 @@ import java.util.List;
 
 // store has a 0$ entry fee
 public class Store extends Place{
+
+    private StoreAction storeAction;
     @Override
     void onEnter(User user) throws IOException {
-        List<String> mainMenuChoices = new ArrayList<>(Arrays.asList("Buy","Sell","Leave"));
+        List<StoreAction> mainMenuChoices = new ArrayList<>(Arrays.asList(StoreAction.BUY, StoreAction.SELL, StoreAction.LEAVE));
         // loop infinitely until the user selects the leave action
         // print the menu to read a StoreAction asking whether to
         // buy, sell, or leave
         while(true) {
-            String choiceOne = ConsoleUtils.printMenuToConsole("Store Main Menu", mainMenuChoices, true);
+            StoreAction choiceOne = ConsoleUtils.printMenuToConsole("Store Main Menu", mainMenuChoices, true);
 
-            if (choiceOne.equals("Buy")) {
-                while (true){
-                    // check if the user is able to buy anything at the store
-                    boolean canBuy = false;
-                    for (Item item : Item.values()){
-                        if (item.getValue() <= user.getBalance()){
-                            canBuy = true;
+            switch (choiceOne){
+                case BUY:
+                    while (true) {
+                        // check if the user is able to buy anything at the store
+                        boolean canBuy = false;
+                        for (Item item : Item.values()) {
+                            if (item.getValue() <= user.getBalance()) {
+                                canBuy = true;
+                            }
                         }
-                    }
-                    if (user.getInventory().size() == 3){
-                        System.out.println("You already have three items in your inventory, you can't buy another item yet.");
-                        break;
-                    }
-                    if (!canBuy){
-                        // send the user back to the menu with three options
-                        System.out.println("With your current finances you are unable to purchase anything at the store.");
-                        break;
-                    }
-                    // the user can buy at least one thing from the store, display the store menu
-                    Item buyItem = ConsoleUtils.printMenuToConsole("Buy Menu", Arrays.asList(Item.values()), true);
-                    // ask the user if they are sure if they want to buy the item
-                    String warningMessage = String.format("Are you sure you would like to buy (%s)", buyItem.toString());
-                    List<String> confirmationChoices = new ArrayList<>(Arrays.asList("Buy", "Cancel"));
-                    String userConfirmation = ConsoleUtils.printMenuToConsole(warningMessage, confirmationChoices, true);
-                    if (userConfirmation.equals("Buy")){
+                        if (user.getInventory().size() == 3) {
+                            System.out.println("You already have three items in your inventory, you can't buy another item yet.");
+                            break;
+                        }
+                        if (!canBuy) {
+                            // send the user back to the menu with three options
+                            System.out.println("With your current finances you are unable to purchase anything at the store.");
+                            break;
+                        }
+                        // the user can buy at least one thing from the store, display the store menu
+                        Item buyItem = ConsoleUtils.printMenuToConsole("Buy Menu", Arrays.asList(Item.values()), true);
+                        // ask the user if they are sure if they want to buy the item
+                        String warningMessage = String.format("Are you sure you would like to buy (%s)", buyItem.toString());
+                        List<String> confirmationChoices = new ArrayList<>(Arrays.asList("Buy", "Cancel"));
+                        String userConfirmation = ConsoleUtils.printMenuToConsole(warningMessage, confirmationChoices, true);
                         // remove the cost of the item from the user's balance
                         user.setBalance(user.getBalance() - buyItem.getValue());
                         // add the item to the user's inventory
@@ -62,45 +64,43 @@ public class Store extends Place{
                         arcade.saveUsersToFile();
                         break;
                     }
-                    else {
-                        break;
-                    }
-                }
 
-            } else if (choiceOne.equals("Sell")) {
-                while (true){
-                    if (user.getInventory() == null){
-                        System.out.println("Make sure you have items in your inventory before you try and sell.");
+                    case SELL:
+                        while (true){
+                            if (user.getInventory() == null){
+                                System.out.println("Make sure you have items in your inventory before you try and sell.");
+                                break;
+                            }
+                            System.out.println("WARNING: If you decide to sell anything you will only get 50% of the item's value back.");
+                            List<Item> userItems = user.getInventory();
+                            Item sellItem = ConsoleUtils.printMenuToConsole("Sell Menu", userItems, true);
+                            List<String> confirmationChoices = new ArrayList<>(Arrays.asList("Sell","Cancel"));
+                            String warningMessage = String.format("Are you sure you want to sell %s, you will only get $%.2f", sellItem.toString(), sellItem.getValue()/2);
+                            String userConfirmation = ConsoleUtils.printMenuToConsole(warningMessage, confirmationChoices, true);
+                            if (userConfirmation.equals("Sell")){
+                                DecimalFormat df = new DecimalFormat();
+                                df.setMaximumFractionDigits(2);
+                                // makes sure there are only two decimal points
+                                double itemValue = Double.parseDouble(df.format(sellItem.getValue()/2));
+                                // remove the item from the user's inventory
+                                user.removeItem(sellItem);
+                                // increase the user balance
+                                user.setBalance(user.getBalance() + itemValue);
+                                // save the user information
+                                arcade.saveUsersToFile();
+                                break;
+                            }
+                            else {
+                                // the user decided to not sell the item, go back to the main store menu
+                                break;
+                            }
+                        }
+                    case LEAVE:
+                        // transition back to the lobby
+                        arcade.transitionArcadeState("Lobby");
                         break;
                     }
-                    System.out.println("WARNING: If you decide to sell anything you will only get 50% of the item's value back.");
-                    List<Item> userItems = user.getInventory();
-                    Item sellItem = ConsoleUtils.printMenuToConsole("Sell Menu", userItems, true);
-                    List<String> confirmationChoices = new ArrayList<>(Arrays.asList("Sell","Cancel"));
-                    String warningMessage = String.format("Are you sure you want to sell %s, you will only get $%.2f", sellItem.toString(), sellItem.getValue()/2);
-                    String userConfirmation = ConsoleUtils.printMenuToConsole(warningMessage, confirmationChoices, true);
-                    if (userConfirmation.equals("Sell")){
-                        DecimalFormat df = new DecimalFormat();
-                        df.setMaximumFractionDigits(2);
-                        // makes sure there are only two decimal points
-                        double itemValue = Double.parseDouble(df.format(sellItem.getValue()/2));
-                        // remove the item from the user's inventory
-                        user.removeItem(sellItem);
-                        // increase the user balance
-                        user.setBalance(user.getBalance() + itemValue);
-                        // save the user information
-                        arcade.saveUsersToFile();
-                        break;
-                    }
-                    else {
-                        // the user decided to not sell the item, go back to the main store menu
-                        break;
-                    }
-                }
-
-            } else if (choiceOne.equals("Leave")) {
-                // transition back to the lobby
-                arcade.transitionArcadeState("Lobby");
+            if (choiceOne == StoreAction.LEAVE){
                 break;
             }
         }
@@ -112,6 +112,19 @@ public class Store extends Place{
     }
 
     enum StoreAction {
-        // I don't know how enums work yet
+        BUY ("Buy"),
+        SELL ("Sell"),
+        LEAVE("Leave");
+
+        private String actionName;
+
+        StoreAction(String actionName){
+            this.actionName = actionName;
+        }
+
+        @Override
+        public String toString(){
+            return actionName;
+        }
     }
 }
