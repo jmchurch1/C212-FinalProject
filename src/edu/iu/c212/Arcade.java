5cb3c5e0464f753c6b7565d53a6fb6391b2f5894
Josh <josh.church.josh@gmail.com>
2021-11-24 23:28:45 -0500

    made IArcade interface, created start of Arcade class

---
+++ src/edu/iu/c212/Arcade.java
@@ -0,0 +1,31 @@
+package edu.iu.c212;
+
+public class Arcade implements IArcade{
+
+
+
+    @Override
+    public List<User> getUserSaveDataFromFile() {
+        return null;
+    }
+
+    @Override
+    public void saveUsersToFile() {
+
+    }
+
+    @Override
+    public void transitionArcadeState(String newPlaceNameToGoTo) {
+
+    }
+
+    @Override
+    public User getUserOnArcadeEntry() {
+        return null;
+    }
+
+    @Override
+    public List<Place> getAllPlaces() {
+        return null;
+    }
+}
---
+++ src/edu/iu/c212/IArcade.java
@@ -0,0 +1,29 @@
+package edu.iu.c212;
+
+public interface IArcade {
+
+    // call FileUtils.getUserDataFromFile, and use System.exit
+    // to exit the program if an exception is thrown
+    List<User> getUserSaveDataFromFile();
+
+    // call FileUtils.writeUserDataToFile to write all users to
+    // the .txt file
+    void saveUsersToFile();
+
+    // transition the currentUser to a new place
+    // conditions: if the user doesn't have enough money to go to
+    // the place, print a warning and go move player to the LOBBY
+    // if the user has enough money subtract the entry fee from the player
+    // save it to the file, then enter the place
+    void transitionArcadeState(String newPlaceNameToGoTo);
+
+    // ask for a username to be entered
+    // if the username isn't contained in the users as read
+    // from users.txt, a new user must be created and saved to users.txt
+    // a welcome message will be printed
+    // if the username exists, print a welcome back message
+    User getUserOnArcadeEntry();
+
+    List<Place> getAllPlaces();
+    // return a list of all the places
+}
