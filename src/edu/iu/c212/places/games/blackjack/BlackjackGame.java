5f25191d0c54d33d0dbea11d1b179e57218c2be6
Josh <josh.church.josh@gmail.com>
2021-11-25 10:13:35 -0500

    created base of main game classes

---
+++ src/edu/iu/c212/places/games/blackjack/BlackjackGame.java
@@ -0,0 +1,5 @@
+package edu.iu.c212.places.games.blackjack;
+import edu.iu.c212.places.games.Game;
+
+public class BlackjackGame extends Game {
+}
---
+++ src/edu/iu/c212/places/games/hangman/HangmanGame.java
@@ -0,0 +1,7 @@
+package edu.iu.c212.places.games.hangman;
+
+import edu.iu.c212.places.games.Game;
+
+public class HangmanGame extends Game implements IHangmanGame{
+
+}
