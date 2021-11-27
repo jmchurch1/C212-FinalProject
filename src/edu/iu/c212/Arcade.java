fca1183ac2b1292c1504b0385dbbb7f2f04810c7
Josh <josh.church.josh@gmail.com>
2021-11-26 22:06:23 -0500

    started getUserOnArcadeEntry

--- src/edu/iu/c212/Arcade.java
+++ src/edu/iu/c212/Arcade.java
@@ -7,6 +7,7 @@ import edu.iu.c212.utils.FileUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Arcade implements IArcade{
 
@@ -42,7 +43,26 @@ public class Arcade implements IArcade{
 
     @Override
     public User getUserOnArcadeEntry() {
-        return null;
+        Scanner sc = new Scanner(System.in);
+        String username = "";
+        while (true) {
+            System.out.println("Please enter the current user's name");
+            username = sc.nextLine();
+            // if the user inputs a name with the length 0 or with a pole in it
+            // do not allow them to input the name
+            if (username.length() == 0 || username.contains("|")) {
+                System.out.println("Make sure the name doesn't contain a | and is at least one character long");
+                continue;
+            }
+            break;
+        }
+        // if the user is in the list of all users then return the user that is found
+        for (User user : allUsers){
+            if (user.getUsername().equals(username))
+                return user;
+        }
+        // otherwise, add a new user to the UserData.txt file
+
     }
 
     @Override
